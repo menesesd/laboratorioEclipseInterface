b0b635e3eba848ad17f3e422e1a3ce5a1d2f1c8a
Daniel Fernando Meneses Jaimes <yasoft51@gmail.com>
2020-04-04 18:29:04 -0500

    corregida ortografia en validacion

--- src/valuebObject/OrdenPedido.java
+++ src/valuebObject/OrdenPedido.java
@@ -75,7 +75,7 @@ public class OrdenPedido {
 	        }
 
 	        if (!isSucursalPrincipal && !isSucursalSecundaria) {
-	            errores += "- Debe elegir almenos una sucursal \n\r";
+	            errores += "- Debe elegir al menos una sucursal \n\r";
 	        }
 
 	        this.nombre = nombre;

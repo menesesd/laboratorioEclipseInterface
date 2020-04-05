5b805a4ecdb79c0e6285653df754839b8d7c9484
Daniel Fernando Meneses Jaimes <yasoft51@gmail.com>
2020-04-04 20:52:06 -0500

    totalmente funcional

--- src/medicamento/VentanaResumen.java
+++ src/medicamento/VentanaResumen.java
@@ -20,81 +20,80 @@ public class VentanaResumen extends JFrame {
 	private JPanel contentPane;
 	private static OrdenPedido ordenPedido;
 
-
 	/**
 	 * Create the frame.
 	 */
 	public VentanaResumen() {
-		
-		VentanaPrincipal ventanaPrincipal= new VentanaPrincipal();
+
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
 		this.ordenPedido = ventanaPrincipal.ordenPedido;
-		
-		setTitle("Pedido al Destribuidor "+this.ordenPedido.getDistribuidor());				
-		
+
+		setTitle("Pedido al Destribuidor " + this.ordenPedido.getDistribuidor());
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 487, 267);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblResumenPedido = new JLabel("Resumen del Pedido");
 		lblResumenPedido.setHorizontalAlignment(SwingConstants.CENTER);
 		lblResumenPedido.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblResumenPedido.setBounds(38, 25, 367, 25);
 		contentPane.add(lblResumenPedido);
-		
+
 		JLabel lblDescripcionPedido = new JLabel("");
 		lblDescripcionPedido.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDescripcionPedido.setBounds(48, 61, 351, 30);
 		contentPane.add(lblDescripcionPedido);
-		
+
 		JLabel lblDireccionFarmacia = new JLabel("New label");
 		lblDireccionFarmacia.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDireccionFarmacia.setBounds(48, 102, 357, 44);
 		contentPane.add(lblDireccionFarmacia);
-		
+
 		JButton btnEnviarPedido = new JButton("Enviar Pedido");
 		btnEnviarPedido.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				System.out.println("Pedido Enviado");								
-				setVisible(false);				
+
+				System.out.println("Pedido Enviado");
+				setVisible(false);
 			}
 		});
 		btnEnviarPedido.setBounds(157, 185, 139, 23);
 		contentPane.add(btnEnviarPedido);
-		
+
 		JButton btnCancelarPedido = new JButton("Cancelar");
 		btnCancelarPedido.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-							
-				setVisible(false);	
+
+				setVisible(false);
 			}
 		});
 		btnCancelarPedido.setBounds(322, 185, 139, 23);
 		contentPane.add(btnCancelarPedido);
-	
 
-		if(this.ordenPedido!=null){
-			String descripcionPedido= "<html>";
-			descripcionPedido+=this.ordenPedido.getCantidad()+" unidades del "+this.ordenPedido.getTipo()+" "+this.ordenPedido.getNombre();	
-			descripcionPedido+= "</html>";
+		if (this.ordenPedido != null) {
+			String descripcionPedido = "<html>";
+			descripcionPedido += this.ordenPedido.getCantidad() + " unidades del " + this.ordenPedido.getTipo() + " "
+					+ this.ordenPedido.getNombre();
+			descripcionPedido += "</html>";
 			lblDescripcionPedido.setText(descripcionPedido);
-			
-			String direccionFarmacia="<html>";
-			if(this.ordenPedido.isSucursalPrincipal()) {
-				direccionFarmacia+="Para la farmacia Pricipal situada en la dirección  Calle de la Rosa n. 28 ";
-				if(this.ordenPedido.isSucursalSecundaria()) {
-					direccionFarmacia+=" y para la farmacia Secundaria situada en la dirección Calle Alcazabilla n. 3.";
-				}								
-			}else if(this.ordenPedido.isSucursalSecundaria()) {
-				direccionFarmacia+=" Para la farmacia Secundaria situada en la dirección Calle Alcazabilla n. 3.";
+
+			String direccionFarmacia = "<html>";
+			if (this.ordenPedido.isSucursalPrincipal()) {
+				direccionFarmacia += "Para la farmacia Pricipal situada en la dirección  Calle de la Rosa n. 28 ";
+				if (this.ordenPedido.isSucursalSecundaria()) {
+					direccionFarmacia += " y para la farmacia Secundaria situada en la dirección Calle Alcazabilla n. 3.";
+				}
+			} else if (this.ordenPedido.isSucursalSecundaria()) {
+				direccionFarmacia += " Para la farmacia Secundaria situada en la dirección Calle Alcazabilla n. 3.";
 			}
-			 direccionFarmacia+="</html>";
-			 
+			direccionFarmacia += "</html>";
+
 			lblDireccionFarmacia.setText(direccionFarmacia);
-		}		
-		
+		}
+
 	}
 }
--- src/valuebObject/OrdenPedido.java
+++ src/valuebObject/OrdenPedido.java
@@ -4,87 +4,97 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OrdenPedido {
-	
-    private String nombre;
-    private String tipo;
-    private int cantidad;
-    private String distribuidor;
-    private boolean isSucursalPrincipal;
-    private boolean isSucursalSecundaria;
-    
+
+	private String nombre;
+	private String tipo;
+	private int cantidad;
+	private String distribuidor;
+	private boolean isSucursalPrincipal;
+	private boolean isSucursalSecundaria;
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public int getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+
 	public String getDistribuidor() {
 		return distribuidor;
 	}
+
 	public void setDistribuidor(String distribuidor) {
 		this.distribuidor = distribuidor;
 	}
+
 	public boolean isSucursalPrincipal() {
 		return isSucursalPrincipal;
 	}
+
 	public void setSucursalPrincipal(boolean isSucursalPrincipal) {
 		this.isSucursalPrincipal = isSucursalPrincipal;
 	}
+
 	public boolean isSucursalSecundaria() {
 		return isSucursalSecundaria;
 	}
+
 	public void setSucursalSecundaria(boolean isSucursalSecundaria) {
 		this.isSucursalSecundaria = isSucursalSecundaria;
 	}
-    
-    
-	  public String validar(String nombre, String tipo, String cantidad, String distribuidor,
-	            boolean isSucursalPrincipal, boolean isSucursalSecundaria) {
-	        String errores = "";
-	    	Pattern p = Pattern.compile("^[a-zA-Z0-9 Ò·ÈÌÛ˙¡…Õ”⁄¸‹—]*$");
-			Matcher  m = p.matcher(nombre);
-	        
-	        if (nombre.trim().equals("") || !m.matches()) {
-	            errores += "- Se debe proporcionar un nombre de medicamento alfanumÈrico \n\r";
-	        }
 
-	        if (tipo.trim().equals("")) {
-	            errores += "- Debe elegir un tipo de medicamento \n\r";
-	        }
+	public String validar(String nombre, String tipo, String cantidad, String distribuidor, boolean isSucursalPrincipal,
+			boolean isSucursalSecundaria) {
+		String errores = "";
+		Pattern p = Pattern.compile("^[a-zA-Z0-9 Ò·ÈÌÛ˙¡…Õ”⁄¸‹—]*$");
+		Matcher m = p.matcher(nombre);
 
-	        try {
-	            this.cantidad = Integer.parseUnsignedInt(cantidad);
-	        } catch (NumberFormatException e) {
-	            errores += "- La cantidad debe ser un valor numÈrico positivo \n\r";
-	        }
+		if (nombre.trim().equals("") || !m.matches()) {
+			errores += "- Se debe proporcionar un nombre de medicamento alfanumÈrico \n\r";
+		}
 
-	        if (distribuidor.trim().equals("")) {
-	            errores += "- Debe elegir un distribuidor \n\r";
-	        }
+		if (tipo.trim().equals("")) {
+			errores += "- Debe elegir un tipo de medicamento \n\r";
+		}
 
-	        if (!isSucursalPrincipal && !isSucursalSecundaria) {
-	            errores += "- Debe elegir al menos una sucursal \n\r";
-	        }
+		try {
+			this.cantidad = Integer.parseUnsignedInt(cantidad);
+		} catch (NumberFormatException e) {
+			errores += "- La cantidad debe ser un valor numÈrico positivo \n\r";
+		}
 
-	        this.nombre = nombre;
-	        this.tipo = tipo;
-	        this.distribuidor = distribuidor;
-	        this.isSucursalPrincipal = isSucursalPrincipal;
-	        this.isSucursalSecundaria = isSucursalSecundaria;
+		if (distribuidor.trim().equals("")) {
+			errores += "- Debe elegir un distribuidor \n\r";
+		}
 
-	        return errores.equals("") ? "success" : errores;
-	    }
+		if (!isSucursalPrincipal && !isSucursalSecundaria) {
+			errores += "- Debe elegir al menos una sucursal \n\r";
+		}
+
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.distribuidor = distribuidor;
+		this.isSucursalPrincipal = isSucursalPrincipal;
+		this.isSucursalSecundaria = isSucursalSecundaria;
+
+		return errores.equals("") ? "success" : errores;
+	}
 
 }
